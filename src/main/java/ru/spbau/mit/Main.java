edc68f7166062afd820e8bda0de9dae2b8a5b988
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-09-25 13:15:19 +0300

    new branch i hope

---
+++ src/main/java/ru/spbau/mit/Main.java
@@ -0,0 +1,10 @@
+package ru.spbau.mit;
+
+/**
+ * Created by Эдгар on 25.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
